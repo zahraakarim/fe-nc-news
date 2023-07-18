134e1bc2c3591c77230119489d1fe4227b7f617d
zahraakarim <nikishakarim@gmail.com>
2023-07-18 13:43:43 +0100

    completed ticket 5 and can view article by id when clicking on the individual articles from article list

--- src/util.js
+++ src/util.js
@@ -1,3 +1,7 @@
 export function dateFormatter(created_at) {
   return new Date(created_at).toLocaleDateString();
 }
+
+export function capitalisedTopic(topic) {
+  return topic.charAt(0).toUpperCase() + topic.slice(1);
+}
